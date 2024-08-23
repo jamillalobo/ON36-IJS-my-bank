2d1b86aa05fb7cf8cac21b8451ddb364bdf8d47a
jamillalobo <lobo.jamilla@gmail.com>
2024-08-23 14:17:14 -0300

    create tests for manager services

--- varzea-bank/src/managers/application/outboundPorts/managers.service.spec.ts
+++ varzea-bank/src/managers/application/outboundPorts/managers.service.spec.ts
@@ -1,18 +1,65 @@
+import { Manager } from '../domain/manager.model';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ManagersService } from './managers.service';
+import { ManagerRepository } from '../inboundPorts/manager.repository';
+import { AccountRepository } from '../../../accounts/application/inboundPorts/account.repository';
 
 describe('ManagersService', () => {
-  let service: ManagersService;
+  test('should create a manager', () => {
+    const managerRepository = new ManagerRepository();
+    const accountRepository = new AccountRepository();
+    const managerService = new ManagersService(accountRepository, managerRepository);
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ManagersService],
-    }).compile();
+    const esperado = {
+      id: 5,
+      name: 'Maria',
+      accounts: [1, 2],
+    };
 
-    service = module.get<ManagersService>(ManagersService);
+    const resultado = managerService.createManager({ name: 'Maria' });
+
+    expect(resultado).toStrictEqual(esperado);
+  }
+  );
+
+  test('should find managers', () => {
+    const managerRepository = new ManagerRepository();
+    const accountRepository = new AccountRepository();
+    const managerService = new ManagersService(accountRepository, managerRepository);
+
+    const esperado = managerRepository.readManagers();
+
+    const resultado = managerService.findAllManagers();
+
+    expect(resultado).toStrictEqual(esperado);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  test('should find manager by id', () => {
+    const managerRepository = new ManagerRepository();
+    const accountRepository = new AccountRepository();
+    jest.spyOn(managerRepository, 'readManagers').mockReturnValue([{ id: 1, name: 'João', accounts: [1, 2] }]);
+
+    const managerService = new ManagersService(accountRepository, managerRepository);
+
+    const esperado = { id: 1, name: 'João', accounts: [1, 2, 3] };
+
+    const resultado = managerService.findManagerById(1);
+
+    expect(resultado).toStrictEqual(esperado);
   });
+
+  test('should delete a manager', () => {
+    const managerRepository = new ManagerRepository();
+    const accountRepository = new AccountRepository();
+    jest.spyOn(managerRepository, 'readManagers').mockReturnValue([{ id: 1, name: 'João', accounts: [1, 2] }]);
+
+    const esperado = jest.spyOn(managerRepository, 'writeManagers').mockImplementation((managers) => {
+      expect(managers).not.toContain({ id: 1, name: 'João', accounts: [1, 2] });
+    });
+
+    const managerService = new ManagersService(accountRepository, managerRepository);
+
+    managerService.deleteManager(1);
+  });
+      
 });
